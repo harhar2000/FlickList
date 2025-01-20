edcdd2e3c097a39527f0b44cb58259a14197a59a
Harry <hmsh2000@gmail.com>
2025-01-20 22:39:22 +0000

    css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
-import "./App.css";
+import "./css/App.css";
+import Favourites from "./pages/Favourites";
 import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
 
 function App() {
-  const movieNumber = 1;
-
-  return <></>;
+  return (
+    <div>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favourites" element={<Favourites />} />
+        </Routes>
+      </main>
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,23 +1,26 @@
-function MovieCard({movie}) {
+import "../css/MovieCard.css";
 
-    function onFavouriteClick() {
-        alert("clicked")
-    }
+function MovieCard({ movie }) {
+  function onFavouriteClick() {
+    alert("clicked");
+  }
 
-    return <div className="movie-card">
-            <div className="movie-poster">
-                <img scr={movie.url} alt={movie.title}/>
-                <div className="movie-overlay">
-                    <button className="favourite-btn" onClick={onFavouriteClick}>
-                       ❤
-                    </button>
-                </div>
-            </div>
-            <div className="movie-info">
-                <h3>{movie.title}</h3>
-                <p>{movie.release_date}</p>
-            </div>
+  return (
+    <div className="movie-card">
+      <div className="movie-poster">
+        <img scr={movie.url} alt={movie.title} />
+        <div className="movie-overlay">
+          <button className="favourite-btn" onClick={onFavouriteClick}>
+            ❤
+          </button>
+        </div>
+      </div>
+      <div className="movie-info">
+        <h3>{movie.title}</h3>
+        <p>{movie.release_date}</p>
+      </div>
     </div>
+  );
 }
 
-export default MovieCard
+export default MovieCard;
