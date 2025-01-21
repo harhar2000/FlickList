8d2f06be0e4821c6042003b1d0473521120c36ff
Harry <hmsh2000@gmail.com>
2025-01-21 16:38:09 +0000

    Finished

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,15 +1,15 @@
-const API_KEY = import.meta.env.VITE_API_KEY;
+const api_key = "";
 const BASE_URL = "https://api.themoviedb.org/3";
 
 export const getPopularMovies = async () => {
-  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${api_key}`);
   const data = await response.json();
   return data.results;
 };
 
 export const searchMovies = async (query) => {
   const response = await fetch(
-    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+    `${BASE_URL}/search/movie?api_key=${api_key}&query=${encodeURIComponent(
       query
     )}`
   );
