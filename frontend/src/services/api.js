5ba55da2aa8669fe097375ca71d189fd97bfd27f
Harry <hmsh2000@gmail.com>
2025-01-21 10:04:07 +0000

    Movie Card not working

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,18 @@
-const API_KEY = process.env.REACT_APP_API_KEY;
-const BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = import.meta.env.VITE_API_KEY;
+const BASE_URL = "https://api.themoviedb.org/";
 
-export { API_KEY, BASE_URL };
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
